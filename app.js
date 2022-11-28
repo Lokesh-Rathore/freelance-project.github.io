d0d7eeeee0ec150b5505c3cf4d895dbdadb06069
Darpanarayan Bahadur <92776555+SkullCreek@users.noreply.github.com>
2022-11-28 23:33:03 +0530

    safety

--- app.js
+++ app.js
@@ -18,3 +18,37 @@ const quantity = () => {
 }
 
 quantity();
+
+document.addEventListener('contextmenu', event => event.preventDefault());
+ 
+document.onkeydown = function (e) {
+
+    // disable F12 key
+    if(e.keyCode == 123) {
+        return false;
+    }
+
+    // disable I key
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 73){
+        return false;
+    }
+
+    // disable J key
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+        return false;
+    }
+
+    // disable U key
+    if(e.ctrlKey && e.keyCode == 85) {
+        return false;
+    }
+}
+
+document.addEventListener("keydown", function(event){
+    var key = event.key || event.keyCode;
+    if (key == 123) {
+        return false;
+    } else if ((event.ctrlKey && event.shiftKey && key == 73) || (event.ctrlKey && event.shiftKey && key == 74)) {
+        return false;
+    }
+}, false);
